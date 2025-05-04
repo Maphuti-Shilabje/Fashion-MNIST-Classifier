39f42d2525308aa25c1d7c9f47b5296551ef6fa1
Maphuti <Fabridgeshilabje01@gmail.com>
2025-05-04 18:16:43 +0200

    Add fashionANN model class with one hidden layer and ReLU activation for FashionMNIST - Uses nn.Flatten, Linear, and ReLU -Output layer produces logits for 10 classes

--- classifier.py
+++ classifier.py
@@ -56,5 +56,51 @@ test_loader = DataLoader(
 )
 
 
+# The neural network model
+
+class fashionANN(nn.Module):
+    def __init__(self, input_size=28*28, hidden_size=128, output_size=10):
+
+        # Network layers definition
+        # The model is a simple feedforward neural network with 3 layers
+        # Input layer: 28x28 pixels (flattened to 784)
+        # Hidden layer 1: 128 neurons
+        # Hidden layer 2: 64 neurons
+        # Output layer: 10 neurons (one for each class)
+        # The model uses ReLU activation function for the hidden layers
+        # and no activation function for the output layer
+      
+        super(fashionANN, self).__init__()
+        self.flatten = nn.Flatten()       # Layer to flatten the 28x28 image
+        self.fc1 = nn.Linear(input_size, hidden_size) # First fully connected layer
+        self.relu = nn.ReLU()             # ReLU activation function
+        self.fc2 = nn.Linear(hidden_size, num_classes) # Output layer
+
+    """ 
+    Forward pass
+    The forward method defines how the input data flows through the network
+    It takes the input tensor x, flattens it, applies the first fully connected layer,
+    applies the ReLU activation function, and then applies the second fully connected layer
+    The output of the forward method is the raw scores (logits) for each class
+    The forward method is called when we pass data through the model
+    The input tensor x is expected to have the shape [batch_size, 1, 28, 28]
+    The output tensor will have the shape [batch_size, num_classes]
+    """
+    def forward(self, x):
+        x = self.flatten(x)    # Shape becomes [batch_size, 784]
+        x = self.fc1(x)         # Shape becomes [batch_size, hidden_size]
+        x = self.relu(x)        # Apply activation, shape remains [batch_size, hidden_size]
+        x = self.fc2(x)         # Shape becomes [batch_size, num_classes] - these are raw scores (logits
+        return x
+
+# Initialize the model
+input_size = 28 * 28 # Flattened size of the input image
+hidden_size = 128   # Number of neurons in the hidden layer
+num_classes = 10    # Number of classes (10 for Fashion MNIST)
+
+model = fashionANN(input_size = input_size , hidden_size = hidden_size, output_size = num_classes).to(DEVICE)
+
+print("Model initialized.")
+print(model)
 
 print("End.")
